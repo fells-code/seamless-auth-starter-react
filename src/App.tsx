4f657f6ab7031e040c8ea90d39ba51dfd209f7be
Brandon Corbett <Bccorb@users.noreply.github.com>
2026-02-07 16:41:14 -0500

    feat: a better home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { AuthProvider, AuthRoutes, useAuth } from "@seamless-auth/react";
+import { AuthProvider } from "@seamless-auth/react";
 
 import "./App.css";
-import Login from "./pages/Login";
 import BetaAccess from "./pages/BetaAccess";
 import MainLayout from "./layouts/Layout";
 import About from "./pages/About";
+import Home from "./pages/Home";
 
 const AUTH_SERVER = "http://localhost:3000/";
 
 function ApplicationRoutes() {
-  const { isAuthenticated } = useAuth();
-
   return (
     <Routes>
-      <Route path="/*" element={<MainLayout />}>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        {isAuthenticated ? (
-          <>
-            <Route path="beta" element={<BetaAccess />} />
-            <Route path="*" element={<BetaAccess />} />
-          </>
-        ) : (
-          <>
-            <Route path="/*" element={<Login />}>
-              <Route index element={<AuthRoutes />} />
-              <Route path="*" element={<AuthRoutes />} />
-            </Route>
-          </>
-        )}
+        <Route path="beta" element={<BetaAccess />} />
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
